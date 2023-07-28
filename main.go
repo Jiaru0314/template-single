cf840c2142e4311dd60b8a5906074a074cffa6b7
zhiqiang.xu <1004971804@qq.com>
2023-07-28 11:06:04 +0800

    fix:代码生成插件 默认注释

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"github.com/Jiaru0314/go_gen_code/codeGenUtil"
+	// "github.com/Jiaru0314/go_gen_code/codeGenUtil"
 	_ "github.com/gogf/gf/contrib/drivers/mysql/v2"
 	"github.com/gogf/gf/v2/os/gctx"
 
@@ -12,7 +12,7 @@ import (
 
 func main() {
 	// 代码生成后去除!!!
-	codeGenUtil.GenALl()
+	// codeGenUtil.GenALl()
 
 	cmd.Main.Run(gctx.GetInitCtx())
 }
